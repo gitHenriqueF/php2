cf70ef7d9cc21fc5d780cafad16bdba9319f95d2
Henrique Fernandes <henrique.fernandes@unidavi.edu.br>
2023-09-19 16:29:07 -0300

    Inplementação alteração

--- lista.compras/index.php
+++ lista.compras/index.php
@@ -9,6 +9,18 @@
             event.preventDefault();
             window.location = "./incluirlista.php";
         }
+
+        function apagar(codigo) {
+            event.preventDefault();
+            if (window.confirm("Confirma execlusão da lista: "+codigo)) {
+                window.location = "./apagalista.php?codigo="+codigo;    
+            }
+        }
+
+        function alterar(codigo) {
+            event.preventDefault();
+            window.location = "./alterarlista.php?codigo="+codigo;    
+        }
     </script>
 </head>
 <body>
@@ -27,7 +39,11 @@
            //apresenta dados das Listas
            echo("<li>");
            echo("<a href='item.php/?lista=".$linha["codigo"]."'>".$linha["codigo"]);
-           echo(" - ".$linha["nome"]."</a></li>");
+           echo(" - ".$linha["nome"]."</a>");
+           echo("&nbsp;&nbsp;");
+           echo("<button onclick='alterar(".$linha["codigo"].")'><img src='pencil.png'></button>");
+           echo("&nbsp;&nbsp;");
+           echo("<button onclick='apagar(".$linha["codigo"].")'><img src='lixeira.png'></button></li>");
        }
        echo("</ul><br>");
        $conn = null;
--- lista.compras/item.php
+++ lista.compras/item.php
@@ -9,6 +9,13 @@
             event.preventDefault();
             window.location = "../incluiritem.php/?lista="+<?=$_GET["lista"]?>;
         }
+
+        function apagar(codigo) {
+            event.preventDefault();
+            if (window.confirm("Confirma exclusão do item: "+codigo)) {
+               window.location = "./apagaitem.php/?codigo="+codigo; 
+            }             
+        }
     </script>
 </head>
 <body>
@@ -28,6 +35,7 @@
                .$linha["datahora"]." - "
                .$linha["descricao"]." - "
                .$linha["quantidade"] 
+               ."&nbsp;&nbsp;<button onclick='apagar(".$linha['codigo'].")')><img src='../lixeira.png' title='Apagar Item'></button>"
                ."</li>");
            }
            echo("</ul><br>");
