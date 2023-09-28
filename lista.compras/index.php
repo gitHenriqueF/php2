49d6cce8540100d0c91baae8b9cb6a96f35e8aeb
Henrique Fernandes <henrique.fernandes@unidavi.edu.br>
2023-09-28 14:09:04 -0300

    Busca na lista

--- lista.compras/index.php
+++ lista.compras/index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -24,18 +24,34 @@
     </script>
 </head>
 <body>
-    <h1>Lista de compras</h1>
+    <h1>Lista de Compras</h1>
+    <form id="fbusca" action="index.php" method="get">
+        <input id="ibusca" name="busca" placeholder="Digite algo para buscar">
+        <button name="bbusca" value="busca">OK</button> 
+    </form>
     <?php 
-    require_once "conexao.php";
-    try {
-       //Realizar Consulta a tabela lista
-       //Prepara o SQL
-        $stmt = $conn->prepare("SELECT * FROM lista");
-       //Executa o SQL
-        $stmt->execute();
-       echo("<br><ul>");
-       //Para Cada linha da Tabela faz
-       foreach ($stmt as $linha) {
+       require_once "conexao.php"; 
+       try {
+           //Realizar Consulta a tabela lista
+           //Prepara o SQL 
+           if (empty($_GET["busca"])) {
+               //busca todos os dados da tabela
+               $stmt = $conn->prepare("SELECT * FROM lista");
+           } else {
+               //busca os dados da tabela, aplicando a busca digitada pelo usuário
+               //Monta SQL com parametros da busca
+               $sql = "SELECT * FROM lista WHERE CODIGO = '".$_GET["busca"]
+                      ."' OR NOME LIKE '%".$_GET["busca"]."%'";
+               //echo $sql;
+               echo "<strong>Pesquisando por: </strong><mark>".$_GET["busca"]."</mark>";
+               $stmt = $conn->prepare($sql);
+           }
+
+           //Executa o SQL
+           $stmt->execute();
+           echo("<br><ul>");
+           //Para Cada linha da Tabela faz
+           foreach ($stmt as $linha) {
            //apresenta dados das Listas
            echo("<li>");
            echo("<a href='item.php/?lista=".$linha["codigo"]."'>".$linha["codigo"]);
